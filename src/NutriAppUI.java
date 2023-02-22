94cd0155662036d98d2835289de44cbf4dec6d18
Jakob Langtry <jjl4287@g.rit.edu>
2023-02-22 17:06:13 -0500

    feat: :white_check_mark: UI filler added, persistence needed
    I added general ui commands, but the input is not saved to anywhere.

---
+++ src/NutriAppUI.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class NutriAppUI {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void displayMenu() {
+        System.out.println("Welcome to NutriApp!");
+        System.out.println("1. Create a new user");
+        System.out.println("2. Add a new meal");
+        System.out.println("3. Add a new workout");
+        System.out.println("4. View user history");
+        System.out.println("5. Exit");
+    }
+
+    public int getUserChoice() {
+        System.out.print("Enter your choice: ");
+        return scanner.nextInt();
+    }
+
+    public void displayUserForm() {
+        System.out.println("Create a new user");
+        System.out.print("Enter your name: ");
+        String name = scanner.next();
+        System.out.print("Enter your height: ");
+        int height = scanner.nextInt();
+        System.out.print("Enter your weight: ");
+        int weight = scanner.nextInt();
+        System.out.print("Enter your birthdate (YYYY-MM-DD): ");
+        String birthdate = scanner.next();
+        // Save the user data to the database
+    }
+
+    public void displayMealForm() {
+        System.out.println("Add a new meal");
+        System.out.print("Enter the name of the meal: ");
+        String mealName = scanner.next();
+        System.out.print("Enter the ingredients (comma separated): ");
+        String ingredients = scanner.next();
+        System.out.print("Enter the calories: ");
+        int calories = scanner.nextInt();
+        // Save the meal data to the database
+    }
+
+    public void displayWorkoutForm() {
+        System.out.println("Add a new workout");
+        System.out.print("Enter the duration (in minutes): ");
+        int duration = scanner.nextInt();
+        System.out.print("Enter the intensity: ");
+        String intensity = scanner.next();
+        System.out.print("Enter the date (YYYY-MM-DD): ");
+        String date = scanner.next();
+        // Save the workout data to the database
+    }
+
+    public void displayUserHistory() {
+        System.out.println("User history");
+        // Retrieve the user history from the database and display it on the console
+    }
+
+    public void displayExitMessage() {
+        System.out.println("Goodbye!");
+    }
+}
--- src/Nutriapp.java
+++ src/Nutriapp.java
@@ -1,3 +1,30 @@
-public class Nutriapp {
-    
+public class NutriApp {
+    private NutriAppUI ui = new NutriAppUI();
+
+    public void run() {
+        int choice = 0;
+        while (choice != 5) {
+            ui.displayMenu();
+            choice = ui.getUserChoice();
+            switch (choice) {
+                case 1:
+                    ui.displayUserForm();
+                    break;
+                case 2:
+                    ui.displayMealForm();
+                    break;
+                case 3:
+                    ui.displayWorkoutForm();
+                    break;
+                case 4:
+                    ui.displayUserHistory();
+                    break;
+                case 5:
+                    ui.displayExitMessage();
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again.");
+            }
+        }
+    }
 }
